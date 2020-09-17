f6abf30c9085a9124bb2bb780092e475c2edbefc
mm_ice <1731442734@qq.com>
2020-09-17 19:21:13 +0800

    全部开发完成

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,14 +1,15 @@
 $(function() {
   var layer = layui.layer
-
   initArtCateList()
-
   // 获取文章分类的列表
   function initArtCateList() {
+    
+
     $.ajax({
       method: 'GET',
       url: '/my/article/cates',
-      success: function(res) {
+      success: function (res) {
+        console.log(res);
         var htmlStr = template('tpl-table', res)
         $('tbody').html(htmlStr)
       }
